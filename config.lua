914d8f2d585acf692ae0d2220c5c6461985fbe00
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-12-06 20:59:58 -0500

    updated

--- config.lua
+++ config.lua
@@ -3,11 +3,11 @@ Config = {}
 Config.HackItem = 'thermite' -- Item required to do the robbery (Change Label to something fitting if you change this)
 Config.HackType = 'greek' -- can be alphabet, numeric, alphanumeric, greek, braille, runes
 Config.HackTime = 25 --how long to do minigame
-Config.HackingTime = 15 --how long for hacking progressbars
+Config.HackingTime = 10 --how long for hacking progressbars
 
 Config.MoneyType = false -- true = cash / false = blackmoney/dirtymoney
-Config.PaymentMin = '10000' -- Min amount they can get
-Config.PaymentMax = '15000' -- Max amount they can get
+Config.PaymentMin = '500' -- Min amount they can get
+Config.PaymentMax = '5000' -- Max amount they can get
 
-Config.Cops = 0 -- Amount of cops needed to do the robbery
-Config.CoolDown = 0 -- How long Cooldown is before robbery can be done again (minutes)
+Config.Cops = 1 -- Amount of cops needed to do the robbery
+Config.CoolDown = 10 -- How long Cooldown is before robbery can be done again (minutes)
